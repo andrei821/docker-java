79a7ca8fdfdae5f4afb1e7bdc2a8cc9629856902
Andrei Manea <andrei@mondoit.ro>
2015-06-05 15:41:39 +0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,6 @@
  */
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Salut Andrei!"); // Display the string.
+        System.out.println("Hello Andrei!"); // Display the string.
     }
 }
